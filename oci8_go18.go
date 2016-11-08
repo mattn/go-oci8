122235c7d97e17f3b399402bbc96bd6ac15dac91
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-11-09 07:35:23 +0900

    add go18 feature

---
+++ oci8_go18.go
@@ -0,0 +1,60 @@
+// +build go1.8
+
+package oci8
+
+import (
+	"database/sql/driver"
+
+	"context"
+)
+
+// Ping implement Pinger.
+func (c *OCI8Conn) Ping(ctx context.Context) error {
+	return c.ping(ctx)
+}
+
+// QueryContext implement QueryerContext.
+func (c *OCI8Conn) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
+	list := make([]namedValue, len(args))
+	for i, nv := range args {
+		list[i] = namedValue(nv)
+	}
+	return c.query(ctx, query, list)
+}
+
+// ExecContext implement ExecerContext.
+func (c *OCI8Conn) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
+	list := make([]namedValue, len(args))
+	for i, nv := range args {
+		list[i] = namedValue(nv)
+	}
+	return c.exec(ctx, query, list)
+}
+
+// PrepareContext implement ConnPrepareContext.
+func (c *OCI8Conn) PrepareContext(ctx context.Context, query string) (driver.Stmt, error) {
+	return c.prepare(ctx, query)
+}
+
+// BeginContext implement ConnBeginContext.
+func (c *OCI8Conn) BeginContext(ctx context.Context) (driver.Tx, error) {
+	return c.begin(ctx)
+}
+
+// QueryContext implement QueryerContext.
+func (s *OCI8Stmt) QueryContext(ctx context.Context, args []driver.NamedValue) (driver.Rows, error) {
+	list := make([]namedValue, len(args))
+	for i, nv := range args {
+		list[i] = namedValue(nv)
+	}
+	return s.query(ctx, list)
+}
+
+// ExecContext implement ExecerContext.
+func (s *OCI8Stmt) ExecContext(ctx context.Context, args []driver.NamedValue) (driver.Result, error) {
+	list := make([]namedValue, len(args))
+	for i, nv := range args {
+		list[i] = namedValue(nv)
+	}
+	return s.exec(ctx, list)
+}

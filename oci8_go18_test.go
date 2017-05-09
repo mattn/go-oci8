5a0b527f3e1d03cc3adac1a77521eccbbe574c25
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-05-09 22:20:02 +0900

    add TestTimeout

--- oci8_go18_test.go
+++ oci8_go18_test.go
@@ -3,8 +3,11 @@
 package oci8
 
 import (
+	"context"
 	"database/sql"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestNamedParam(t *testing.T) {
@@ -12,4 +15,36 @@ func TestNamedParam(t *testing.T) {
 	if "helloworld" != r["MESSAGE"].(string) {
 		t.Fatal("message should be: helloworld", r)
 	}
+}
+
+func TestTimeout(t *testing.T) {
+	db := DB()
+	for i := 0; i < 2000; i++ {
+		db.Exec("insert into foo(c3) values(:1)", i)
+	}
+
+	var wg sync.WaitGroup
+	f := func(wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		stmt, err := db.Prepare(`select * from foo order by c3`)
+		if err != nil {
+			t.Fatal(err)
+		}
+		ctx, _ := context.WithTimeout(context.Background(), 200*time.Millisecond)
+		rows, err := stmt.QueryContext(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer rows.Close()
+		err = ctx.Err()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	for j := 0; j < 10; j++ {
+		wg.Add(1)
+		go f(&wg)
+	}
+	wg.Wait()
 }

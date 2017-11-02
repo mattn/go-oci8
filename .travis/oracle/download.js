73e70e9fc52baed56e26e53fb22c21a913e69490
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-11-02 12:04:57 +0900

    fix download URL

--- .travis/oracle/download.js
+++ .travis/oracle/download.js
@@ -40,4 +40,4 @@ page.settings.userAgent = env['USER_AGENT'];
 
 page.onResourceError = function(error) { system.stderr.writeLine(JSON.stringify(error)); phantom.exit(1); }
 page.onLoadFinished = javascriptRequired;
-page.open('https://edelivery.oracle.com/akam/otn/linux/oracle11g/xe/' + env['ORACLE_FILE']);
+page.open('https://download.oracle.com/otn/linux/oracle11g/xe/' + env['ORACLE_FILE']);

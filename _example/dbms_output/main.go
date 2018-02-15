f31a814667d004fc331d21aeb2f8c2b881758d5b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-02-16 00:44:37 +0900

    add example for dbms_output

---
+++ _example/dbms_output/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-oci8"
+)
+
+func main() {
+	db, err := sql.Open("oci8", "scott/tiger@XE")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(`BEGIN DBMS_OUTPUT.ENABLE(10000); END;`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(`BEGIN DBMS_OUTPUT.PUT_LINE('hello'); END;`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var lines string
+	var status int64
+	_, err = db.Exec(`BEGIN DBMS_OUTPUT.GET_LINE(:lines, :status); END;`,
+		sql.Named("lines", sql.Out{Dest: &lines}),
+		sql.Named("status", sql.Out{Dest: &status, In: true}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(lines, nlines)
+	/*
+		for rows.Next() {
+			var i int
+			err = rows.Scan(&i)
+			if err != nil {
+				log.Fatal(err)
+			}
+			println(i)
+		}
+	*/
+}

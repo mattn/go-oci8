21fdd563523d5537536370239c32733fd8d098f8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-03-02 00:05:23 +0900

    fix test

--- oci8_go19_test.go
+++ oci8_go19_test.go
@@ -14,7 +14,7 @@ func TestOutputBind(t *testing.T) {
 	s2 := 11
 	s3 := false
 	s4 := uint64(12)
-	_, err := db.Exec(`begin  :a := 42; :b := 'ddddd' ; :c := 2; :d := unsigned(4); end;`,
+	_, err := db.Exec(`begin  :a := 42; :b := 'ddddd' ; :c := 2; :d := 18446744073709551615; end;`,
 		sql.Named("a", sql.Out{Dest: &s2}),
 		sql.Named("b", sql.Out{Dest: &s1}),
 		sql.Named("c", sql.Out{Dest: &s3}),
@@ -32,7 +32,7 @@ func TestOutputBind(t *testing.T) {
 	if !s3 {
 		t.Fatalf("want %v but %v", true, s3)
 	}
-	if s4 != uint64(4) {
-		t.Fatalf("want %v but %v", uint64(4), s4)
+	if s4 != uint64(18446744073709551615) {
+		t.Fatalf("want %v but %v", uint64(18446744073709551615), s4)
 	}
 }

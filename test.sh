513a7e385e5094230cb9c33b81f5f33000340b12
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-08-28 00:13:17 -0700

    Updated test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,20 @@
 set -e
 
-echo installing build tools
+echo "installing build tools"
 apt-get -qq -y update 2>&1 > /dev/null
 apt-get -qq -y install git pkg-config gcc 2>&1 > /dev/null
-echo installing go1.10
+
+
+echo "installing go1.10"
 wget -q https://dl.google.com/go/go1.10.linux-amd64.tar.gz -O /tmp/go1.10.linux-amd64.tar.gz
 mkdir -p /usr/local
 tar xf /tmp/go1.10.linux-amd64.tar.gz -C /usr/local
-
 export PATH=/usr/local/go/bin:$PATH
+export GOROOT=/usr/local/go
+mkdir -p /usr/local/goFiles
+export GOPATH=/usr/local/goFiles
+
+echo "setting up Oracle"
 export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 export LD_LIBRARY_PATH=/u01/app/oracle/product/11.2.0/xe/lib
 export ORACLE_SID=XE
@@ -22,13 +28,10 @@ CREATE USER scott IDENTIFIED BY tiger DEFAULT TABLESPACE users TEMPORARY TABLESP
 GRANT connect, resource, create view, create synonym TO scott;
 SQL
 
-echo go get -d github.com/mattn/go-oci8
-go get -d github.com/mattn/go-oci8
-cp -r ${TESTDIR} -T .
 
-echo building
-cd $(go env GOPATH)/src/github.com/mattn/go-oci8
-
+echo "creating oci8.pc"
+cd GOPATH
+export PKG_CONFIG_PATH=GOPATH
 cat > oci8.pc <<PKGCONFIG
 Name: oci8
 Description: Oracle Call Interface
@@ -37,5 +40,10 @@ Cflags: -I${ORACLE_HOME}/rdbms/public
 Libs: -L${ORACLE_HOME}/lib -Wl,-rpath,${ORACLE_HOME}/lib -lclntsh
 PKGCONFIG
 
-export PKG_CONFIG_PATH=.
+
+echo "getting go-oci8"
+go get -d github.com/mattn/go-oci8
+
+
+echo "testing go-oci8"
 go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger

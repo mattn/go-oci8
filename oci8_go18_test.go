4e33345018b7a006088202831215e7e0b74e7520
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-08-28 02:04:06 -0700

    Updated testing

--- oci8_go18_test.go
+++ oci8_go18_test.go
@@ -11,40 +11,44 @@ import (
 )
 
 func TestNamedParam(t *testing.T) {
-	r := sqlstest(DB(), t, "select :foo||:bar as message from dual", sql.Named("foo", "hello"), sql.Named("bar", "world"))
+	r := sqlstest(TestDB, t, "select :foo||:bar as message from dual", sql.Named("foo", "hello"), sql.Named("bar", "world"))
 	if "helloworld" != r["MESSAGE"].(string) {
 		t.Fatal("message should be: helloworld", r)
 	}
 }
 
 func TestTimeout(t *testing.T) {
-	db := DB()
 	for i := 0; i < 2000; i++ {
-		db.Exec("insert into foo(c3) values(:1)", i)
+		TestDB.Exec("insert into foo(c3) values(:1)", i)
 	}
 
 	var wg sync.WaitGroup
-	f := func(wg *sync.WaitGroup) {
+	wg.Add(10)
+
+	f := func() {
 		defer wg.Done()
 
-		stmt, err := db.Prepare(`select * from foo order by c3`)
+		stmt, err := TestDB.Prepare(`select * from foo order by c3`)
 		if err != nil {
 			t.Fatal(err)
 		}
 		ctx, _ := context.WithTimeout(context.Background(), 200*time.Millisecond)
 		rows, err := stmt.QueryContext(ctx)
-		if err != nil {
+		if err != nil && err.Error() != "ORA-01013: user requested cancel of current operation\n" {
 			t.Fatal(err)
 		}
-		defer rows.Close()
+		if err != nil && rows != nil {
+			defer rows.Close()
+		}
 		err = ctx.Err()
 		if err != nil && err != context.DeadlineExceeded {
 			t.Fatal(err)
 		}
 	}
+
 	for j := 0; j < 10; j++ {
-		wg.Add(1)
-		go f(&wg)
+		go f()
 	}
+
 	wg.Wait()
 }

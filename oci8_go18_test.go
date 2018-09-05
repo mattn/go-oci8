40bbbd227d58ca0603f8062dc263a242954088e7
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-09-05 16:57:32 -0700

    Added tests

--- oci8_go18_test.go
+++ oci8_go18_test.go
@@ -3,11 +3,8 @@
 package oci8
 
 import (
-	"context"
 	"database/sql"
-	"sync"
 	"testing"
-	"time"
 )
 
 func TestNamedParam(t *testing.T) {
@@ -19,44 +16,4 @@ func TestNamedParam(t *testing.T) {
 	if "helloworld" != r["MESSAGE"].(string) {
 		t.Fatal("message should be: helloworld", r)
 	}
-}
-
-func TestTimeout(t *testing.T) {
-	if TestDisableDatabase {
-		t.SkipNow()
-	}
-
-	for i := 0; i < 2000; i++ {
-		TestDB.Exec("insert into foo(c3) values(:1)", i)
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(10)
-
-	f := func() {
-		defer wg.Done()
-
-		stmt, err := TestDB.Prepare(`select * from foo order by c3`)
-		if err != nil {
-			t.Fatal(err)
-		}
-		ctx, _ := context.WithTimeout(context.Background(), 200*time.Millisecond)
-		rows, err := stmt.QueryContext(ctx)
-		if err != nil && err.Error() != "ORA-01013: user requested cancel of current operation\n" {
-			t.Fatal(err)
-		}
-		if err != nil && rows != nil {
-			defer rows.Close()
-		}
-		err = ctx.Err()
-		if err != nil && err != context.DeadlineExceeded {
-			t.Fatal(err)
-		}
-	}
-
-	for j := 0; j < 10; j++ {
-		go f()
-	}
-
-	wg.Wait()
 }

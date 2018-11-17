e5eb230727d09f8fcd9871d0ccc53c69cf488f5a
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-11-16 17:46:09 -0800

    Added Example_sqlRowid

--- exampleSql_test.go
+++ exampleSql_test.go
@@ -266,12 +266,6 @@ func Example_sqlInsert() {
 		return
 	}
 
-	err = db.Close()
-	if err != nil {
-		fmt.Println("Close error is not nil:", err)
-		return
-	}
-
 	fmt.Println(rowsAffected)
 
 	// output: 1
@@ -417,4 +411,138 @@ func Example_sqlManyInserts() {
 	fmt.Println(count)
 
 	// output: 3
+}
+
+func Example_sqlRowid() {
+	// Example shows a few ways to get rowid
+
+	// For testing, check if database tests are disabled
+	if oci8.TestDisableDatabase || oci8.TestDisableDestructive {
+		fmt.Println(1)
+		return
+	}
+
+	oci8.OCI8Driver.Logger = log.New(os.Stderr, "oci8 ", log.Ldate|log.Ltime|log.LUTC|log.Llongfile)
+
+	var openString string
+	// [username/[password]@]host[:port][/instance_name][?param1=value1&...&paramN=valueN]
+	if len(oci8.TestUsername) > 0 {
+		if len(oci8.TestPassword) > 0 {
+			openString = oci8.TestUsername + "/" + oci8.TestPassword + "@"
+		} else {
+			openString = oci8.TestUsername + "@"
+		}
+	}
+	openString += oci8.TestHostValid
+
+	// A normal simple Open to localhost would look like:
+	// db, err := sql.Open("oci8", "127.0.0.1")
+	// For testing, need to use additional variables
+	db, err := sql.Open("oci8", openString)
+	if err != nil {
+		fmt.Printf("Open error is not nil: %v", err)
+		return
+	}
+	if db == nil {
+		fmt.Println("db is nil")
+		return
+	}
+
+	// defer close database
+	defer func() {
+		err = db.Close()
+		if err != nil {
+			fmt.Println("Close error is not nil:", err)
+		}
+	}()
+
+	// create table
+	tableName := "E_ROWID_" + oci8.TestTimeString
+	query := "create table " + tableName + " ( A INTEGER )"
+	ctx, cancel := context.WithTimeout(context.Background(), 55*time.Second)
+	_, err = db.ExecContext(ctx, query)
+	cancel()
+	if err != nil {
+		fmt.Println("ExecContext error is not nil:", err)
+		return
+	}
+
+	// insert row and get rowid from returning
+	var rowid1 string // rowid will be put into here
+	var result sql.Result
+	query = "insert into " + tableName + " ( A ) values (:1) returning rowid into :rowid1"
+	ctx, cancel = context.WithTimeout(context.Background(), 55*time.Second)
+	result, err = db.ExecContext(ctx, query, 1, sql.Named("rowid1", sql.Out{Dest: &rowid1}))
+	cancel()
+	if err != nil {
+		fmt.Println("ExecContext error is not nil:", err)
+		return
+	}
+
+	// get rowid from LastInsertId
+	var rowid2 string // rowid will be put into here
+	var id int64
+	id, err = result.LastInsertId()
+	if err != nil {
+		fmt.Println("LastInsertId error is not nil:", err)
+		return
+	}
+	rowid2 = oci8.GetLastInsertId(id)
+
+	// select rowid
+	var rowid3 string // rowid will be put into here
+	var rows *sql.Rows
+	query = "select rowid from " + tableName
+	ctx, cancel = context.WithTimeout(context.Background(), 55*time.Second)
+	defer cancel()
+	rows, err = db.QueryContext(ctx, query)
+	if err != nil {
+		fmt.Println("QueryContext error is not nil:", err)
+		return
+	}
+	if !rows.Next() {
+		fmt.Println("no Next rows")
+		return
+	}
+
+	err = rows.Scan(&rowid3)
+	if err != nil {
+		fmt.Println("Scan error is not nil:", err)
+		return
+	}
+
+	if rows.Next() {
+		fmt.Println("has Next rows")
+		return
+	}
+
+	err = rows.Err()
+	if err != nil {
+		fmt.Println("Err error is not nil:", err)
+		return
+	}
+	err = rows.Close()
+	if err != nil {
+		fmt.Println("Close error is not nil:", err)
+		return
+	}
+
+	// drop table
+	query = "drop table " + tableName
+	ctx, cancel = context.WithTimeout(context.Background(), 55*time.Second)
+	_, err = db.ExecContext(ctx, query)
+	cancel()
+	if err != nil {
+		fmt.Println("ExecContext error is not nil:", err)
+		return
+	}
+
+	if len(rowid1) != len(rowid2) || len(rowid2) != len(rowid3) {
+		fmt.Println("rowid len is not equal", rowid1, rowid2, rowid3)
+		return
+	}
+
+	fmt.Println("done")
+
+	// output: done
 }

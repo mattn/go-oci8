68c63731b0abd9662bb7f166f8851bd20b5fa842
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-12-18 18:50:12 -0800

    test to Go 1.11.4 & 1.10.7

--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ apt-get -qq -y install git pkg-config gcc 2>&1 > /dev/null
 echo "installing go"
 export PATH_SAVE=${PATH}
 cd /tmp/
-wget -nv https://dl.google.com/go/go1.11.linux-amd64.tar.gz
-wget -nv https://dl.google.com/go/go1.10.4.linux-amd64.tar.gz
+wget -nv https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
+wget -nv https://dl.google.com/go/go1.10.7.linux-amd64.tar.gz
 wget -nv https://dl.google.com/go/go1.9.7.linux-amd64.tar.gz
 
 mkdir -p /usr/local/goFiles1.11.x

8a8b27cc24f2b8c7d9b4adf466e702fd12f243aa
Luis Gabriel Gomez <luis.gg1990@gmail.com>
2020-12-17 00:21:27 -0300

    Add test cleanup script

---
+++ test_cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf /usr/local/go1.15.x || true
+rm -rf /usr/local/go1.14.x || true
+rm -rf /usr/local/go1.13.x || true
+rm -rf /usr/local/go1.12.x || true
+rm -rf /usr/local/go1.11.x || true
+rm -rf /usr/local/go1.10.x || true
+rm -rf /usr/local/go1.9.x || true
+rm -rf /usr/local/pkg_config || true

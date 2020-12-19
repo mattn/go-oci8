6319cb2902d501ef4e517ef84fbe0cb4360b8f68
Luis Gabriel Gomez <luis.gg1990@gmail.com>
2020-12-19 20:49:38 -0300

    Refactor code into functions. Implement feedback suggestions

--- test.sh
+++ test.sh
@@ -1,6 +1,30 @@
-
-#!/bin/bash
 set -e
+
+setup_go_dist () {
+    VERSION_MAJOR_MINOR=$1
+    VERSION_RELEASE=$2
+    VERSION_FULL="${1}.${2}"
+
+    FILE=go${VERSION_FULL}.linux-amd64.tar.gz
+    [ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/${FILE}
+
+    mkdir -p /usr/local/goFiles${VERSION_MAJOR_MINOR}.x
+    tar -xf /tmp/${FILE}
+    mv /tmp/go /usr/local/go${VERSION_MAJOR_MINOR}.x
+}
+
+test_go_dist () {
+    VERSION_MAJOR_MINOR=$1
+
+    echo "testing go-oci8 Go ${VERSION_MAJOR_MINOR}.x"
+    export PATH=/usr/local/go${VERSION_MAJOR_MINOR}.x/bin:${PATH_SAVE}
+    export GOROOT=/usr/local/go${VERSION_MAJOR_MINOR}.x
+    export GOPATH=/usr/local/goFiles${VERSION_MAJOR_MINOR}.x
+    mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
+    cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
+
+    go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
+}
 
 echo "installing build tools"
 apt-get -qq -y update  2>&1 > /dev/null
@@ -9,48 +33,13 @@ apt-get -qq -y install git pkg-config gcc wget  2>&1 > /dev/null
 echo "installing go"
 cd /tmp/
 
-FILE=go1.15.6.linux-amd64.tar.gz
-[ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz
-FILE=go1.14.13.linux-amd64.tar.gz
-[ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/go1.14.13.linux-amd64.tar.gz
-FILE=go1.13.15.linux-amd64.tar.gz
-[ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/go1.13.15.linux-amd64.tar.gz
-FILE=go1.12.17.linux-amd64.tar.gz
-[ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/go1.12.17.linux-amd64.tar.gz
-FILE=go1.11.13.linux-amd64.tar.gz
-[ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/go1.11.13.linux-amd64.tar.gz
-FILE=go1.10.8.linux-amd64.tar.gz
-[ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/go1.10.8.linux-amd64.tar.gz
-FILE=go1.9.7.linux-amd64.tar.gz
-[ ! -e "${FILE}" ] && wget -nv https://dl.google.com/go/go1.9.7.linux-amd64.tar.gz
-
-mkdir -p /usr/local/goFiles1.15.x
-tar -xf /tmp/go1.15.6.linux-amd64.tar.gz
-mv /tmp/go /usr/local/go1.15.x
-
-mkdir -p /usr/local/goFiles1.14.x
-tar -xf /tmp/go1.14.13.linux-amd64.tar.gz
-mv /tmp/go /usr/local/go1.14.x
-
-mkdir -p /usr/local/goFiles1.13.x
-tar -xf /tmp/go1.13.15.linux-amd64.tar.gz
-mv /tmp/go /usr/local/go1.13.x
-
-mkdir -p /usr/local/goFiles1.12.x
-tar -xf /tmp/go1.12.17.linux-amd64.tar.gz
-mv /tmp/go /usr/local/go1.12.x
-
-mkdir -p /usr/local/goFiles1.11.x
-tar -xf /tmp/go1.11.13.linux-amd64.tar.gz
-mv /tmp/go /usr/local/go1.11.x
-
-mkdir -p /usr/local/goFiles1.10.x
-tar -xf /tmp/go1.10.8.linux-amd64.tar.gz
-mv /tmp/go /usr/local/go1.10.x
-
-mkdir -p /usr/local/goFiles1.9.x
-tar -xf /tmp/go1.9.7.linux-amd64.tar.gz
-mv /tmp/go /usr/local/go1.9.x
+setup_go_dist "1.15" "6"
+setup_go_dist "1.14" "13"
+setup_go_dist "1.13" "15"
+setup_go_dist "1.12" "17"
+setup_go_dist "1.11" "13"
+setup_go_dist "1.10" "8"
+setup_go_dist "1.9" "7"
 
 echo "starting Oracle"
 /usr/sbin/startup.sh
@@ -92,69 +81,10 @@ PKGCONFIG
 
 export PATH_SAVE=${PATH}
 
-echo "testing go-oci8 Go 1.15.x"
-export PATH=/usr/local/go1.15.x/bin:${PATH_SAVE}
-export GOROOT=/usr/local/go1.15.x
-export GOPATH=/usr/local/goFiles1.15.x
-mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
-cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
-
-go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
-
-echo "testing go-oci8 Go 1.14.x"
-export PATH=/usr/local/go1.14.x/bin:${PATH_SAVE}
-export GOROOT=/usr/local/go1.14.x
-export GOPATH=/usr/local/goFiles1.14.x
-mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
-cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
-
-go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
-
-echo "testing go-oci8 Go 1.13.x"
-export PATH=/usr/local/go1.13.x/bin:${PATH_SAVE}
-export GOROOT=/usr/local/go1.13.x
-export GOPATH=/usr/local/goFiles1.13.x
-mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
-cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
-
-go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
-
-
-echo "testing go-oci8 Go 1.12.x"
-export PATH=/usr/local/go1.12.x/bin:${PATH_SAVE}
-export GOROOT=/usr/local/go1.12.x
-export GOPATH=/usr/local/goFiles1.12.x
-mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
-cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
-
-go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
-
-
-echo "testing go-oci8 Go 1.11.x"
-export PATH=/usr/local/go1.11.x/bin:${PATH_SAVE}
-export GOROOT=/usr/local/go1.11.x
-export GOPATH=/usr/local/goFiles1.11.x
-mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
-cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
-
-go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
-
-
-echo "testing go-oci8 Go 1.10.x"
-export PATH=/usr/local/go1.10.x/bin:${PATH_SAVE}
-export GOROOT=/usr/local/go1.10.x
-export GOPATH=/usr/local/goFiles1.10.x
-mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
-cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
-
-go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
-
-
-echo "testing go-oci8 Go 1.9.x"
-export PATH=/usr/local/go1.9.x/bin:${PATH_SAVE}
-export GOROOT=/usr/local/go1.9.x
-export GOPATH=/usr/local/goFiles1.9.x
-mkdir -p ${GOPATH}/src/github.com/mattn/go-oci8
-cp -r ${TESTDIR}/* ${GOPATH}/src/github.com/mattn/go-oci8/
-
-go test -v github.com/mattn/go-oci8 -args -disableDatabase=false -hostValid ${DOCKER_IP} -username scott -password tiger
+test_go_dist "1.15"
+test_go_dist "1.14"
+test_go_dist "1.13"
+test_go_dist "1.12"
+test_go_dist "1.11"
+test_go_dist "1.10"
+test_go_dist "1.9"
--- test_cleanup.sh
+++ test_cleanup.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
-rm -rf /usr/local/go1.15.x || true
-rm -rf /usr/local/go1.14.x || true
-rm -rf /usr/local/go1.13.x || true
-rm -rf /usr/local/go1.12.x || true
-rm -rf /usr/local/go1.11.x || true
-rm -rf /usr/local/go1.10.x || true
-rm -rf /usr/local/go1.9.x || true
-rm -rf /usr/local/pkg_config || true
+rm -rf /usr/local/go1.15.x
+rm -rf /usr/local/go1.14.x
+rm -rf /usr/local/go1.13.x
+rm -rf /usr/local/go1.12.x
+rm -rf /usr/local/go1.11.x
+rm -rf /usr/local/go1.10.x
+rm -rf /usr/local/go1.9.x
+rm -rf /usr/local/pkg_config
